3e95c22ed077f318a27e4b480eef53cb4c589b71
mariambatool221 <mariambatool2212@gmail.com>
2019-03-15 13:49:00 +0500

    firstCommit

--- tweets/src/twitter/Extract.java
+++ tweets/src/twitter/Extract.java
@@ -1,6 +1,4 @@
-/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
+
 package twitter;
 
 import java.time.Instant;
